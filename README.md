c3c24665b53706776f54f2a9b9a56299847236f0
FabioBiao <32443868+FabioBiao@users.noreply.github.com>
2017-10-02 21:31:23 +0100

    Rest API tested

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Php-RestApi
 basic backend Php Rest API
+
+creators: https://www.codeofaninja.com/2017/02/create-simple-rest-api-in-php.html
---
+++ tests/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+//http://localhost/Php-RestApi/product/read.php
+//http://localhost/Php-RestApi/product/read_one.php?id=60
+//http://localhost/Php-RestApi/product/search.php?s=shirt
+//http://localhost/Php-RestApi/product/read_paging.php
+
+
+//http://localhost/Php-RestApi/category/read.php
+echo "test";
+
+//phpinfo();
+?>
